eabfc2aa7f89670141529c2fe16c3e04b0560fdf
vito <vitor.cynh@gmail.com>
2026-03-06 15:57:59 -0300

    5st commit

---
+++ exercises/Exercise7.py
@@ -0,0 +1,8 @@
+alunos = {}
+for i in range(3):
+    nome = input("Digite o nome do aluno: ")
+    nota = input('Digite a nota do aluno: ')
+    
+    alunos += alunos.append(nome, nota)    
+
+print(alunos)
